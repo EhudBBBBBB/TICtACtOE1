2d8b820cfe205a65a5d9df930b8c4e75e5fa8d02
ehud <ehudis2016@gmail.com>
2024-11-14 09:24:05 +0200

    TicTacToe

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.tictactoe;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -12,6 +16,72 @@ public class MainActivity extends AppCompatActivity {
     String turn;
     String [] [] board;
     int count;
+    private Thread view;
+
+    private void onNewGame(){
+    board = new String[3][3];
+    for(int row=0;row<3;row++){
+        for(int col=0;col<3;col++){
+            board[row][col]=new String();
+            turn="x";
+            count=0;
+        }
+    }
+}
+
+    private void handleClick(int row, int col, int id) {
+        if (board[row][col].equals("")) {
+        board[row][col] = turn;
+        Button btn = findViewById(id);
+        btn.setText(turn);
+        onTurnEnd();
+    }
+    }
+
+    private void onTurnEnd() {
+        if(isWineer())
+            endGame(turn+"Won !");
+        else if(count==9)
+            endGame("Draw !");
+        else
+            turn=turn.equals("x")?"o":"x";
+    }
+
+    private void endGame(String s) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("More info");
+        String msg = "this is the message body";
+        builder.setMessage(msg);
+        AlertDialog.Builder builder1 = builder.setPositiveButton("exit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //Exit Handling
+            }
+        });
+        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //Cancel Handling
+            }
+        });
+        AlertDialog dialog = builder.show();
+    }
+
+    private boolean isWineer() {
+        for(int row=0;row<3;row++){
+            if(board[row][0].equals(board[row][1]) && board[row][1].equals(board[row][2]) && !board[row][0].equals(""))
+                return true;
+        }
+        for(int col=0;col<3;col++){
+            if(board[0][col].equals(board[1][col]) && board[1][col].equals(board[2][col]) && !board[0][col].equals(""))
+                return true;
+        }
+        if(board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]) && !board[0][0].equals(""))
+            return true;
+        if(board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0]) && !board[0][2].equals(""))
+            return true;
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +96,26 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
+        onNewGame();
+    }
+    public void onButtoncClick(View view) {
+        if (view.getId()==R.id.btn_00)
+            handleClick(0,0,R.id.btn_00);
+        if(view.getId()==R.id.btn_01)
+            handleClick(0,1,R.id.btn_01);
+        if(view.getId()==R.id.btn_02)
+            handleClick(0,2,R.id.btn_02);
+        if(view.getId()==R.id.btn_10)
+            handleClick(1,0,R.id.btn_10);
+        if(view.getId()==R.id.btn_11)
+            handleClick(1,1,R.id.btn_11);
+        if(view.getId()==R.id.btn_12)
+            handleClick(1,2,R.id.btn_12);
+        if(view.getId()==R.id.btn_20)
+            handleClick(2,0,R.id.btn_20);
+        if(view.getId()==R.id.btn_21)
+            handleClick(2,1,R.id.btn_21);
+        if(view.getId()==R.id.btn_22)
+            handleClick(2,2,R.id.btn_22);
     }
 }
